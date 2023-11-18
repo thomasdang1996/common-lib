59144055be9faeb10fe5ffa428a940ed66c51b04
thomasdang1996 <thomasdang1996@gmail.com>
2023-11-18 21:35:53 +0100

    fixed TransactionRepository

--- src/main/java/com/dang/commonlib/transaction/TransactionRepository.java
+++ src/main/java/com/dang/commonlib/transaction/TransactionRepository.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
     @Query("SELECT t FROM Transaction t WHERE t.messageId=?1")
-    @Modifying
     Optional<Transaction> findByMessageId(UUID messageId);
 
     @Query("SELECT t.replyMessage FROM Transaction t WHERE t.messageId=?1")
